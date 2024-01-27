e1694608e0d3fc68f73a43c530e488ad01662900
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2024-01-27 18:41:46 -0300

    feat: add initial discovery navbar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,17 @@
-export default function Dashboard() {
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
+export default function Dashboard() {
   return (
-    <>
-      <h1>Discovery</h1>
-    </>
+    <div className="px-8 py-4 w-full">
+      <div className="flex gap-4 items-center justify-between">
+        <h1 className="font-bold text-2xl">Discovery</h1>
+        <Input type="search" placeholder="Search for channels or shops" />
+        <div className="flex gap-2">
+          <Button variant="outline">Create a channel</Button>
+          <Button variant="outline">Create a shop</Button>
+        </div>
+      </div>
+    </div>
   );
 }
