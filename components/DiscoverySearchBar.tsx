64787d45a853938ca37ccea6144608a2c70c222d
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2024-02-10 15:58:45 -0300

    feat: add tooltip to buttons

--- components/DiscoverySearchBar.tsx
+++ components/DiscoverySearchBar.tsx
@@ -3,6 +3,12 @@ import { ChannelCreateDialog } from './ChannelCreateDialog';
 import { ShopCreateDialog } from './ShopCreateDialog';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './ui/tooltip';
 
 export interface Props {}
 
@@ -15,20 +21,44 @@ export const DiscoverySearchBar = ({}: Props) => {
       <h1 className="font-bold text-2xl">Discovery</h1>
       <Input type="search" placeholder="Search for channels or shops" />
       <div className="flex gap-2">
-        <ChannelCreateDialog
-          triggerElement={
-            <Button disabled={hasChannel} variant="outline">
-              Create a channel
-            </Button>
-          }
-        />
-        <ShopCreateDialog
-          triggerElement={
-            <Button disabled={hasShop} variant="outline">
-              Create a shop
-            </Button>
-          }
-        />
+        {hasChannel ? (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="secondary">Create a channel</Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>
+                  Currently you can create only 1 channel. We&apos;re lifting
+                  the limit soon!
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        ) : (
+          <ChannelCreateDialog
+            triggerElement={<Button variant="outline">Create a channel</Button>}
+          />
+        )}
+        {hasShop ? (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="secondary">Create a shop</Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>
+                  Currently you can create only 1 shop. We&apos;re lifting the
+                  limit soon!
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        ) : (
+          <ShopCreateDialog
+            triggerElement={<Button variant="outline">Create a shop</Button>}
+          />
+        )}
       </div>
     </div>
   );
